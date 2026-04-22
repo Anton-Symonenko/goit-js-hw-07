714d545466aa5908de6b30594972b3c23ce9d9b5
Anton-Symonenko <gan4yrnuk24@gmail.com>
2026-04-22 20:35:53 +0200

    task 1 done

--- js/task-1.js
+++ js/task-1.js
@@ -2,3 +2,9 @@ const categories = document.querySelectorAll('li.item');
 
 console.log(`Number of categories: ${categories.length}`);
 
+categories.forEach(category => {
+    const title = category.querySelector('h2').textContent;
+    const categoriesCount = category.querySelectorAll('li').length;
+    console.log(`Category: ${title}`)
+    console.log(`Elements: ${categoriesCount}`)
+});
