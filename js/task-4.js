b58c5f10ff3ad82620c52fa5c8971aa88502c80a
Anton-Symonenko <gan4yrnuk24@gmail.com>
2026-04-26 19:47:56 +0200

    task 4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", evt => {
+    evt.preventDefault();
+    const { email, password } = form.elements;
+    const user = {
+        email: email.value.trim(),
+        password: password.value.trim(),
+    };
+    if (user.email === "" || user.password === "") {
+        alert("All form fields must be filled in");
+        return;
+    }
+    console.log(user);
+    form.reset();
+});
 
